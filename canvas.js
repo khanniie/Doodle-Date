565844e306aae9fa4f69533f2370589fd91012ea
Connie Ye <happymecye@gmail.com>
2016-07-22 02:52:05 -0400

    file reference can now be moved around!

--- canvas.js
+++ canvas.js
@@ -455,7 +455,7 @@ $("#refUrl").keypress(function(e){
     
        var image = new Image();
        image.id = "theimage";
-       
+       image.ondragstart = function() { return false; };
        
        
        image.onerror = function(){
@@ -494,6 +494,7 @@ $("#refUrl").keypress(function(e){
 
            
        file.appendChild(image);  
+               file.setAttribute("width", 250);
                
                file.style.display="block";
            }
@@ -542,3 +543,41 @@ document.getElementById("closeRef").addEventListener("click",function(){
         file.removeChild(a);
   
 });
+
+
+//from jsfiddle onlines
+var selected = null, // Object of the element to be moved
+    x_pos = 0, y_pos = 0, // Stores x & y coordinates of the mouse pointer
+    x_elem = 0, y_elem = 0; // Stores top, left values (edge) of the element
+
+// Will be called when user starts dragging an element
+function _drag_init(elem) {
+    // Store the object of the element which needs to be moved
+    selected = elem;
+    x_elem = x_pos - selected.offsetLeft;
+    y_elem = y_pos - selected.offsetTop;
+}
+
+// Will be called when user dragging an element
+function _move_elem(e) {
+    x_pos = document.all ? window.event.clientX : e.pageX;
+    y_pos = document.all ? window.event.clientY : e.pageY;
+    if (selected !== null) {
+        selected.style.left = (x_pos - x_elem) + 'px';
+        selected.style.top = (y_pos - y_elem) + 'px';
+    }
+}
+
+// Destroy the object when we are done
+function _destroy() {
+    selected = null;
+}
+
+// Bind the functions...
+document.getElementById('fileRef').onmousedown = function () {
+    _drag_init(this);
+    return false;
+};
+
+document.onmousemove = _move_elem;
+document.onmouseup = _destroy;
